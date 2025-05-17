9f57445ef975f314735d5cb70e9824515f70c3ed
phol <edwinrojastaquiri@gmail.com>
2025-05-17 17:43:06 -0500

    Cambios de nombre

--- client/src/components/ui/Logo.tsx
+++ client/src/components/ui/Logo.tsx
@@ -7,10 +7,10 @@ const Logo: React.FC = () => {
       <Link href="/">
         <div className="flex items-center cursor-pointer">
           <span className="bg-primary rounded-full w-8 h-8 flex items-center justify-center text-white font-bold mr-2">
-            F
+            Y
           </span>
           <span className="text-xl font-bold">
-            foodi<span className="text-primary">slice</span>
+            YAMI<span className="text-primary">CORP</span>
           </span>
         </div>
       </Link>
